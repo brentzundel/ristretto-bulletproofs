6277163eafb06927aa44267f144cdc39b1d6e45b
Oleg Andreev <oleganza@gmail.com>
2018-04-12 11:44:03 -0700

    force config rayon

--- benches/bulletproofs.rs
+++ benches/bulletproofs.rs
@@ -14,7 +14,12 @@ use ristretto_bulletproofs::{PedersenGenerators, Generators};
 use ristretto_bulletproofs::ProofTranscript;
 use ristretto_bulletproofs::RangeProof;
 
+extern crate rayon;
+
 fn bench_create_helper(n: usize, c: &mut Criterion) {
+    /// XXX force init of the thread pool for Rayon to see if it changes anything
+    rayon::ThreadPoolBuilder::new().num_threads(6).build_global();
+
     c.bench_function(&format!("create_rangeproof_n_{}", n), move |b| {
         let generators = Generators::new(PedersenGenerators::default(), n, 1);
         let mut rng = OsRng::new().unwrap();
